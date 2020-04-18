c43d59f5a1589049466548d20fd7420245d20813
avigehlot05@gmail.com <avigehlot05@gmail.com>
2020-04-18 21:34:25 +0530

    Login/Signup api

--- api/functions/util/config.js
+++ api/functions/util/config.js
@@ -1,10 +1,3 @@
 module.exports = {
-    apiKey: "AIzaSyBRAfAXmfAiiRrte25Lm91roywbWPW79Z4",
-    authDomain: "covid19-792cf.firebaseapp.com",
-    databaseURL: "https://covid19-792cf.firebaseio.com",
-    projectId: "covid19-792cf",
-    storageBucket: "covid19-792cf.appspot.com",
-    messagingSenderId: "1091249098778",
-    appId: "1:1091249098778:web:a874377c467886052341a3",
-    measurementId: "G-45YY9LC4GX"
+  
 }
